ee5ec4700d400be8719e1e307eaefe1f9b0adf04
irufano <irfanhidayatms@gmail.com>
2025-02-07 18:16:37 +0700

    update sample test

--- aigoo_fusion/test/test_chat.py
+++ aigoo_fusion/test/test_chat.py
@@ -1,4 +1,3 @@
-import pprint
 import os
 from dotenv import load_dotenv
 
@@ -11,62 +10,67 @@ from aigoo_fusion.chat.models.openai.openai_usage_tracker import openai_usage_tr
 from aigoo_fusion.chat.tools.tool import Tool
 from aigoo_fusion.exception.aigoo_exception import AIGooException
 
-env_file = os.getenv("ENV_FILE", ".env") # Default to .env if ENV_FILE is not set
+env_file = os.getenv("ENV_FILE", ".env")  # Default to .env if ENV_FILE is not set
 load_dotenv(env_file)
 
 
 def _set_env(var: str):
-	os.environ[var] = os.getenv(var, "None")
-	# print(f"{var} set!")
+    os.environ[var] = os.getenv(var, "None")
+    # print(f"{var} set!")
+
 
 _set_env("OPENAI_API_KEY")
 
+
 # Test tools
 def test_tools():
-	# llm
-	llm = OpenAIModel("gpt-4o-mini", config=OpenAIConfig(temperature=0.7))
+    # llm
+    llm = OpenAIModel("gpt-4o-mini", config=OpenAIConfig(temperature=0.7))
 
-	# Initialize framework
-	framework = AIGooChat(llm, system_message="You are a helpful assistant.")
+    # Initialize framework
+    framework = AIGooChat(llm, system_message="You are a helpful assistant.")
 
-	# Define a sample tool
-	@Tool()
-	def get_current_weather(location: str, unit: str = "celsius") -> str:
-		return f"The weather in {location} is 22 degrees {unit}"
+    # Define a sample tool
+    @Tool()
+    def get_current_weather(location: str, unit: str = "celsius") -> str:
+        return f"The weather in {location} is 22 degrees {unit}"
 
-	@Tool()
-	def get_time(location: str) -> str:
-		return f"The time in {location} is 09:00 AM"
+    @Tool()
+    def get_time(location: str) -> str:
+        return f"The time in {location} is 09:00 AM"
 
-	tools = [get_current_weather, get_time]
+    tools = [get_current_weather, get_time]
 
-	# Register tool
-	framework.register_tool(tools)
+    # Register tool
+    framework.register_tool(tools)
 
-	try:
-		# Example conversation with tool use
-		messages = [
-			Message(role=Role.USER, content="What's the weather like in London and what time?")
-		]
+    try:
+        # Example conversation with tool use
+        messages = [
+            Message(
+                role=Role.USER,
+                content="What's the weather like in London and what time?",
+            )
+        ]
 
-		response = framework.generate_with_tools(messages)
-		print(f"\n>> {response.result.content}\n")
+        response = framework.generate_with_tools(messages)
+        print(f"\n>> {response.result.content}\n")
 
-	except AIGooException as e:
-		print(f"Error: {e}")
+    except AIGooException as e:
+        print(f"Error: {e}")
+
 
 # Test prompt
-info="""
-Irufano adalah seorang sofware engineer.
-Dia berasal dari Indonesia.
-""" 
 def test_prompt():
+    info = """
+	Irufano adalah seorang sofware engineer.
+	Dia berasal dari Indonesia.
+	"""
+
     # Configuration
-    config = OpenAIConfig(
-        temperature=0.7
-    )
+    config = OpenAIConfig(temperature=0.7)
     llm = OpenAIModel(model="gpt-4o-mini", config=config)
-    
+
     SYSTEM_PROMPT = """Answer any user questions based solely on the data below:
     <data>
     {info}
@@ -76,20 +80,19 @@ def test_prompt():
 
     # Initialize framework
     framework = AIGooChat(llm, system_message=SYSTEM_PROMPT, input_variables=["info"])
-    
+
     try:
         # Example conversation with tool use
-        messages = [
-            Message(role=Role.USER, content="apa ibukota china")
-        ]
+        messages = [Message(role=Role.USER, content="apa ibukota china")]
         with openai_usage_tracker() as usage:
             response = framework.generate(messages, info=info)
             print(f"\n>> {response.result.content}\n")
             print(f"\nUsage:\n{usage}\n")
-        
+
     except AIGooException as e:
         print(f"{e}")
-		
+
+
 if __name__ == "__main__":
-# test_tools()
+    # test_tools()
     test_prompt()
--- aigoo_fusion/test/test_history.py
+++ aigoo_fusion/test/test_history.py
@@ -1,30 +1,45 @@
 import asyncio
-from datetime import datetime
 import pprint
 import random
 import time
+
 from aigoo_fusion.chat.messages.message import Message
 from aigoo_fusion.chat.messages.role import Role
 from aigoo_fusion.flow.aigoo_flow import AIGooFlow
-from aigoo_fusion.flow.helper.chat_memory.chat_memory import ChatMemory
 from aigoo_fusion.flow.node.node import END, START
+from aigoo_fusion.flow.state.memory_manager import MemoryManager
 from aigoo_fusion.flow.state.workflow_state import WorkflowState
 
+# Initialize memory manager
+memory_manager = MemoryManager(extend_list=True)
 
-chat_memory = ChatMemory()
+# Create workflow with memory manager
+state = {
+    "messages": [],
+    "skill": {"programming": []},
+    "auth": {
+        "name": "irufano",
+        "company": "gokil",
+    },
+}
+workflow = AIGooFlow(state, memory=memory_manager)
 
-# Workflow
-workflow = AIGooFlow({
-	"messages": [] ,
-})
 
 async def main(state: WorkflowState) -> dict:
-	messages = state.get("messages", [])
-	responses = ["Hello", "Wowww", "Amazing", "Gokil", "Good game well played", "Selamat pagi", "Maaf aku tidak tahu"]
-	random_answer = random.choice(responses)
-	ai_message = Message(role=Role.ASSISTANT, content=random_answer)
-	messages.append(ai_message)
-	return {"messages": messages}
+    messages = state.get("messages", [])
+    responses = [
+        "Hello",
+        "Wowww",
+        "Amazing",
+        "Gokil",
+        "Good game well played",
+        "Selamat pagi",
+        "Maaf aku tidak tahu",
+    ]
+    random_answer = random.choice(responses)
+    ai_message = Message(role=Role.ASSISTANT, content=random_answer)
+    messages.append(ai_message)
+    return {"messages": messages}
 
 
 # Add nodes
@@ -32,41 +47,55 @@ workflow.add_node("main", main)
 workflow.add_edge(START, "main")
 workflow.add_edge("main", END)
 
-async def call_workflow(question: str, thread_id: str):
-	try:
-		message = Message(role=Role.USER, content=question)
 
-		async with chat_memory.intercept(thread_id=thread_id, message=message) as (messages, result_call):
-			res = await workflow.execute({
-				"messages": messages
-			})
-			# must call this back 
-			result_call['messages'] = res['messages']
+async def call_workflow(
+    question: str,
+    thread_id: str,
+    name: str,
+    company: str,
+    coding: str,
+):
+    try:
+        message = Message(role=Role.USER, content=question)
+        messages = [message]
+        auth = {"name": name, "company": company}
+        programming = {"programming": [{"name": coding}]}
+        res = await workflow.execute(
+            {"messages": messages, "auth": auth, "skill": programming}, thread_id
+        )
 
-		history = chat_memory.get_thread_history(thread_id=thread_id, max_length=None)
-		return res, history
-	except Exception as e:
-		raise e
+        return res
+    except Exception as e:
+        raise e
 
 
 async def chat_terminal():
-	print("Welcome to the Chat Terminal! Type 'exit' to quit.")
-	print("Use one digit number on thread id for simplicity testing, i.e: thread_id: 1")
+    print("Welcome to the Chat Terminal! Type 'exit' to quit.")
+    print(
+        "Use one digit number on thread id for simplicity testing, i.e: thread_id: 1\n"
+    )
 
-	while True:
-		thread_id = input("thread_id: ")
-		user_input = input("You: ")
+    while True:
+        thread_id = input("thread_id: ")
+        name = input("name: ")
+        company = input("company: ")
+        coding = input("coding: ")
+        user_input = input("You: ")
 
-		if user_input.lower() == 'exit':
-			print("Chatbot: Goodbye!")
-			break
+        if user_input.lower() == "exit":
+            print("Chatbot: Goodbye!")
+            break
 
-		response, history = await call_workflow(user_input.lower(), thread_id)
-		time.sleep(0.5) # Simulate a small delay for realism
-		print(f"\nChatbot: {response['messages'][-1].content}\n")
-		print(f"History: ")
-		for msg in history:
-			print(f"\t{msg}")
+        response = await call_workflow(
+            user_input.lower(), thread_id, name, company, coding
+        )
+        time.sleep(0.5)  # Simulate a small delay for realism
+        print(f"\nChatbot: {response['messages'][-1].content}\n")
+        pprint.pp(response)
+        # print("History: ")
+        # for msg in history:
+        #     print(f"\t{msg}")
+
 
 if __name__ == "__main__":
-	asyncio.run(chat_terminal())
+    asyncio.run(chat_terminal())
